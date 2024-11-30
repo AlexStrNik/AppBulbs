2bbc2662fcf1afaa06a3e30bce438e33eafe8c88
alexstrnik <alex.str.nik@gmail.com>
2024-11-30 14:04:13 +0300

    Initial Commit

---
+++ HappyNY2025/HappyNY2025App.swift
@@ -0,0 +1,17 @@
+//
+//  HappyNY2025App.swift
+//  HappyNY2025
+//
+//  Created by Aleksandr Strizhnev on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HappyNY2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
